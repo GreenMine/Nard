4c41bdc93cd2ae235302bab4276e9fba8ad1ad5a
GreenMine <greenmine94@gmail.com>
2020-09-19 13:53:45 +0300

    End

--- Nard.h
+++ Nard.h
@@ -1,9 +1,10 @@
 #pragma once
+#include <tuple>
 
 enum Color {
-	Void = 0,
-	Black,
-	White
+	Black = 0,
+	White,
+	Void
 };
 
 class Hole {
@@ -22,30 +23,51 @@ public:
 	}
 };
 
+
+using namespace std;
 class Nard {
-private:
-	Hole table[2][10];
 
 public:
 	Nard() {
-		table[0][0] = Hole(15, Color::White);
-		table[1][0] = Hole(15, Color::Black);
+		table[0][0] = Hole(15, Color::Black);
+		table[1][0] = Hole(15, Color::White);
 	}
 
-	void move(int i, int j, int to) {
+	bool move(int i, int j, int to) {
 		Hole *current = &table[i][j];
-		if(j + to > 9) {
-			i = i == 0 ? 1 : 0;
-			j = j + to - 10;
-			std::cout << j << std::endl;
-		} else {
-			j += to;
-		}
-		Hole *to_hole = &table[i][j];
+		Hole *to_hole;
 
-		current->amount--;
+		j += to;
+		if(j > 9) {
+			if(is_winner_zone(current->color)) {
+				amount_winners_checker[current->color]++;
+				goto END;
+			}
+			if(current->color == i) {
+				return false;
+			}
+			i = !i;
+			j = j - 10;
+		}
+
+		to_hole = &table[i][j];
+
+		if(current->color != to_hole->color && to_hole->color != Color::Void)
+			return false;
+
 		to_hole->amount++;
 		to_hole->color = current->color;
+END:
+		current->amount--;
+		return true;
+	}
+
+	Color get_winner() {
+		if(amount_winners_checker[0] < 15 && amount_winners_checker[1] < 15) {
+			return Color::Void;
+		}
+		return amount_winners_checker[0] > amount_winners_checker[1] ? Color::Black : Color::White;
+
 	}
 
 	Hole *get_hole(int i, int j) {
@@ -53,10 +75,11 @@ public:
 	}
 
 	void draw() {
-		std::cout << "┏━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┓\n";
+		for(int i = 10; i > 0; i--) cout << " " << i << "  "; cout << endl;
+		cout << "┏━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┳━━━┓\n";
 
 		for(int i = 0; i < 2; i++) {
-			std::cout << "┃";
+			cout << "┃";
 			if(i == 0) {
 				for(int j = 9; j >= 0; j--) {
 					draw_current(i, j);
@@ -67,29 +90,48 @@ public:
 				}
 			}
 
-			std::cout << '\n';
+			cout << " Выведенных: " << amount_winners_checker[i] << endl;
 
 			if(i == 0) {
 				for(int k = 0; k < 7; k++) {
-					std::cout << "┃                   ┃                   ┃\n";
+					cout << "┃                   ┃                   ┃\n";
 				}
 			}
 		}
 
-		std::cout << "┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛\n";
+		cout << "┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛\n";
+		for(int i = 1; i <= 10; i++) cout << "  " << i << " "; cout << endl;
+	}
+
+private:
+	Hole table[2][10];
+	int amount_winners_checker[2] = {14, 0};
+
+	bool is_winner_zone(Color checker_color) {
+		int winning_row = checker_color == Color::Black ? 0 : 1;
+		int sum = amount_winners_checker[checker_color];
+		for(int i = 5; i < 10; i++) {
+			if(checker_color == table[winning_row][i].color) {
+				sum += table[winning_row][i].amount;
+			}
+		}
+		return sum == 15;
 	}
 
 	void draw_current(int i, int j) {
 		Hole current = table[i][j];
 		if(current.amount == 0) {
-			std::cout << "   ";
+			cout << "   ";
 		} else {
 			if(current.amount < 10)
-				std::cout << ' ';
-			std::cout << current.amount << (current.color == Color::Black ? "ч" : "б");
+				cout << ' ';
+			cout << current.amount << (current.color == Color::Black ? "ч" : "б");
 		}
-		std::cout << "┃";
+		cout << "┃";
+	}
+
+	tuple<int, int> get_position(int i, int j, int step) {
+		return make_tuple(i, j);
 	}
 
 };
-
--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "Nard.h"
 #include <unistd.h>
-#include <cstdlib>
 #include <tuple>
 
 using namespace std;
@@ -12,38 +11,80 @@ int main() {
 
 	srand(time(NULL));
 
+	system("clear");
+
 	Nard nard = Nard();
 
-	Color current_turn = Color::Black;
+	tuple<int, int> generated_numbers = generate_cubes();
+	int black_c = get<0>(generated_numbers), white_c = get<1>(generated_numbers);
+
+	cout << "Через жеребьевку будет определ тот, кто будет ходить первым." << endl;
+	cout << "Черному вылало выпало " << black_c << "\nБелому выпало " << white_c << endl;
+
+	Color current_turn = black_c >= white_c ? Color::Black : Color::White;
 
 	while(true) {
 
 		int i, j, turn_length;
-		nard.draw();
+		bool is_second = false, additional_turn = false;
 
 		tuple<int, int> cubes = generate_cubes();
 		int cube1 = get<0>(cubes), cube2 = get<1>(cubes);
+
+		if(cube1 == cube2)
+			additional_turn = true;
+
+BAD_ANSWER:
+		nard.draw();
+		cout << "Сейчас ход: " << (current_turn == Color::Black ? "черный" : "белый") << endl;
+
 		cout << "Вам выпало: " << cube1 << " и " << cube2 << endl;
-		if(cube1 == cube2) {
+		if(additional_turn) {
 			cout << "Вам доступен дополнительный ход!" << endl;
 		}
 
-BAD_ANSWER:
 		cout << "Выберите строку и колонну шашки, которую хотите переместить: ";
 		cin >> i >> j;
-		Hole *choosed = nard.get_hole(i - 1, j - 1);
-		if(choosed->color != current_turn) {
+		if(nard.get_hole(i - 1, j - 1)->color != current_turn) {
 			cout << "ВЫ ВЫБРАЛИ НЕ ВАШУ КЛЕТКУ ЛИБО ПУСТУЮ КЛЕТКУ!" << endl;
 			goto BAD_ANSWER;
 		}
-		cout << "ВЫберите длину хода(1 или 2 куб): ";
-		cin >> turn_length;
+		if(!is_second) {
+			cout << "Выберите длину хода(1 или 2 куб): ";
+			cin >> turn_length;
+		} else {
+			turn_length = turn_length == 1 ? 2 : 1;
+		}
 
-		nard.move(i - 1, j - 1, turn_length == 1 ? cube1 : cube2);
+		if(!nard.move(i - 1, j - 1, turn_length == 1 ? cube1 : cube2)) {
+			system("clear");
+			cout << "Вы не можете сделать шаг на данную клетку." << endl;
+			goto BAD_ANSWER;
+		}
+
+		Color winner = nard.get_winner();
+		if(winner != Color::Void) {
+			cout << "Победил " << (winner == Color::Black ? "черный" : "белый") << "! Поздравляем!!!" << endl;
+			break;
+		}
+
+		if(!is_second) {
+			is_second = true;
+			system("clear");
+			goto BAD_ANSWER;
+		}
+
+		if(additional_turn) {
+			is_second = false;
+			additional_turn = false;
+			system("clear");
+			goto BAD_ANSWER;
+		}
 
 		current_turn = current_turn == Color::Black ? Color::White : Color::Black;
 
-//		system("clear");
+		system("clear");
+
 	}
 }
 
